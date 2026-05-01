7a8e89a060407adbc89d8253dab46ec2032c41c1
Akainor <akainor@hotmail.com>
2026-05-01 14:49:08 -0300

    0.0.3 - Corrige cors para put e delete

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ app.use(bodyParser.json());
 // Libera acesso externo (CORS)
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
+});
+app.options('*', (req, res) => {
+    res.sendStatus(200);
 });
 // Função para ler arquivo
 function readNotes() {
